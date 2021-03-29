34d97f7b04a9423bee8b16083dd3e8fbee83471f
Shahzad Afridi <shahzadahmadafridi@gmail.com>
2021-03-29 16:47:59 +0500

    Fixed AttendenceInResponse.kt issue

--- app/src/main/java/com/empire/adminschool/Models/attendence/AttendenceOutResponse.kt
+++ app/src/main/java/com/empire/adminschool/Models/attendence/AttendenceOutResponse.kt
@@ -1,12 +1,13 @@
 package com.empire.adminschool.Models.attendence
 
 data class AttendenceOutResponse(
-    val message: String,
-    val narration: String,
-    val sms: Boolean,
-    val sms_text: String,
-    val formate: String,
-    val mobile: String,
-    val type: String,
-    val status: Int
+        val format: String,
+        val message: String,
+        val mobile: String,
+        val narration: String,
+        val sms: Boolean,
+        val sms_text: String,
+        val status: Int,
+        val student_name: String,
+        val type: String
 )
